43ea9871cc5dcafc244618ec687bca70c3ed071b
decun <diazfarid10@gmail.com>
2023-04-20 14:35:01 -0400

    Resolucion Certamen 1 FaridDiaz 201830024-1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
-import koa from 'koa'
-import bodyParser from 'koa-body'
-import router from './routes/index'
+import koa from 'koa';
+import bodyParser from 'koa-body';
+import router from './routes/index';
+import usersjson from './dataset/users_info.json';
 
-const app = new koa()
-const port = 3000
+const app = new koa();
+const port = 3000;
 
-app.use(bodyParser({ multipart: true, urlencoded: true }))
-app.use(router.routes())
+
+
+let users = [];
+
+try {
+  users = usersjson;
+} catch (err) {
+  console.error(`Error loading users data: ${err}`);
+}
+
+async function searchUsersByName(ctx, next) {
+  const { name } = ctx.params;
+  const filteredUsers = users.filter(user => user.name.toLowerCase().includes(name.toLowerCase()));
+  ctx.status = 200;
+  ctx.body = { users: filteredUsers };
+  return next();
+}
+
+
+
+app.use(bodyParser({ multipart: true, urlencoded: true }));
+app.use(router.routes());
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-})
+  console.log(`Server is running on port ${port}`);
+});
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,22 @@
+
 import Router from 'koa-router'
 import getHealth from './health/health'
 
-const router = new Router()
+const router = new Router();
 
-router.get('/health', getHealth)
+async function searchUsersByName(ctx, next) {
+    const usersjson = require('../dataset/users_info.json');
+    const { name } = ctx.params;
+    const filteredUsers = usersjson.filter(user => user.name.toLowerCase().includes(name.toLowerCase()));
+    ctx.status = 200;
+    ctx.body = { users: filteredUsers };
+    return next();
+  }
+  
+router.get('/api/users/:name', searchUsersByName);
 
-export default router
+router.get('/health', getHealth);
+
+router.get('/api/users/:name', searchUsersByName);
+
+export default router;
